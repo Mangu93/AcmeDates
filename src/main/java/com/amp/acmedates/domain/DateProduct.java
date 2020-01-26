a960347466499408d4bc97b523d0066061791489
Adrian Marin <adrianmarinportillo@gmail.com>
2020-01-26 12:13:40 +0100

    Added classes

---
+++ src/main/java/com/amp/acmedates/domain/DateProduct.java
@@ -0,0 +1,87 @@
+package com.amp.acmedates.domain;
+
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.io.Serializable;
+import java.time.ZonedDateTime;
+import java.util.Objects;
+
+@Entity
+@Table(name = "date_product")
+public class DateProduct implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @NotNull
+    @Column(name = "name", unique = true)
+    private String name;
+
+    @NotNull
+    @Column(name = "delivery_date", nullable = false)
+    private ZonedDateTime deliveryDate;
+
+    @Column(name = "declared_value", nullable = false)
+    private Double declaredValue;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ZonedDateTime getDeliveryDate() {
+        return deliveryDate;
+    }
+
+    public void setDeliveryDate(ZonedDateTime deliveryDate) {
+        this.deliveryDate = deliveryDate;
+    }
+
+    public Double getDeclaredValue() {
+        return declaredValue;
+    }
+
+    public void setDeclaredValue(Double declaredValue) {
+        this.declaredValue = declaredValue;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        DateProduct that = (DateProduct) o;
+        return id == that.id &&
+            name.equals(that.name) &&
+            deliveryDate.equals(that.deliveryDate) &&
+            declaredValue.equals(that.declaredValue);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, deliveryDate, declaredValue);
+    }
+
+    @Override
+    public String toString() {
+        return "DateProduct{" +
+            "id=" + id +
+            ", name='" + name + '\'' +
+            ", deliveryDate=" + deliveryDate +
+            ", declaredValue=" + declaredValue +
+            '}';
+    }
+}
